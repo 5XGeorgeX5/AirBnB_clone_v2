1d84c183f38ae4c08e72be00580550996a547f37
George Raafat <georgexraafat@gmail.com>
2023-10-05 12:00:54 -0700

    task 4

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+deletes out-of-date archives, using the function do_clean.
+"""
+from fabric.api import local, run, env
+
+
+env.hosts = ['100.26.166.229', '34.202.157.96']
+env.user = 'ubuntu'
+
+
+def do_clean(number=0):
+    """deletes out-of-date archives"""
+    number = int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local("cd versions; ls -t | tail +{} | xargs rm -f".format(number))
+    path = '/data/web_static/releases'
+    run('cd {}; ls -t | tail +{} | xargs rm -rf'.format(path, number))
