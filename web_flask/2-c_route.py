4ddc143b83095c85b1ad51d3711dcc5c4e7e6357
George Raafat <georgexraafat@gmail.com>
2023-10-20 02:30:04 -0700

    doing some task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,21 +4,22 @@
 from flask import Flask
 from markupsafe import escape
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     "displays Hello HBNB!"
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     "displays HBNB"
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c(text):
     "displays C followed by the value of the text variable"
     return f"C {escape(text.replace('_', ' '))}"
