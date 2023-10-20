1da1df3175754ead87beeed0702bbb1bfa098eee
George Raafat <georgexraafat@gmail.com>
2023-10-20 02:17:13 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    "displays Hello HBNB!"
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    "displays HBNB"
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    "displays C followed by the value of the text variable"
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
